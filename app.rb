5f90be6c948374e6077890dd94c1a045627f13e2
Oskar <oskar@pilborg.se>
2019-08-20 16:10:12 +0200

    Routes
    Började på routes och inloggning. Det var lite problem som förhoppningsvis kan lösas onsdag.

--- app.rb
+++ app.rb
@@ -2,66 +2,12 @@ enable :sessions
 
 class App < Sinatra::Base
 
-include AppModule
-    helpers do
-        def get_error_login()
-            msg = session[:msg_login_failed].dup
-            session[:msg_login_failed] = nil
-            return msg
-        end
-        def get_error_create()
-            msg = session[:msg_create_failed].dup
-            session[:msg_create_failed] = nil
-            return msg
-        end
-        def get_error_review()
-            msg = session[:msg_review_failed].dup
-            session[:msg_review_failed] = nil
-            return msg
-        end
-        def get_validate_error_login()
-            msg = session[:validate_login_error_msg].dup
-            session[:validate_login_error_msg] = nil
-            return msg
-        end
-        def get_no_info_error_login()
-            msg = session[:no_info_error_msg].dup
-            session[:no_info_error_msg] = nil
-            return msg
-        end 
-    end
-    # configure do
-    #     set :unsecured_paths, ['/', '/login', '/new', '/create']
-    # end
-
-    # before do
-    #     unless settings.unsecured_paths.include?(request.path)
-    #         if session[:user_id].nil?
-    #             redirect('/')
-    #         end
-    #     end
-    # end
-
     get("/") do
-    slim(:login)
+        slim(:login)
     end
 
-    get("/layout") do
-    slim(:layout)
-    end
-
-    #Ny sida för elever
-    get("/new") do
-    slim(:new)
-    end
-
-    #profil för folk på sidan
-    get("/profile") do
-    slim(:profile)
-    end
-
-    get("/logout") do
-    slim(:logout)
+    get("/register") do
+        slim(:loggedin)
     end
 
     post("/login") do
@@ -83,5 +29,23 @@ include AppModule
         end
     end
 
+    post("/search") do
+        search = params['search']
+        
+    end
+
+    get("/klass/:klass") do
+        slim(:klass)
+    end
+
+    #Ny sida för en ny elev
+    get("/new") do
+        slim(:new)
+    end
+
+    #profil för folk på sidan
+    get("/profile") do
+        slim(:profile)
+    end
 end
 
