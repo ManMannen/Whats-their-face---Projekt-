37fee9ef54b85d2d2f4f06344c0292c4e19b5782
Oskar <oskar@pilborg.se>
2019-08-20 12:18:16 +0200

    Start med sinatra

--- controller.rb
+++ controller.rb
@@ -2,3 +2,16 @@ require 'slim'
 require 'sinatra'
 require 'BCrypt'
 require_relative 'app.rb'
+
+
+get '/' do
+    slim :index
+end
+
+post 'search' do
+    search = params['search']
+end
+
+get 'results/:search' do
+    slim :results
+end
