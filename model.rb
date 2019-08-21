e04a5ad37cc3faaf7f88da92dcb93bf0b1b91b90
Oskar <oskar@pilborg.se>
2019-08-21 13:10:05 +0200

    Yeetus Deleteus

--- model.rb
+++ model.rb
@@ -7,8 +7,8 @@ module AppModule
     end
     
     def login(params)
-    db = database()
-    result = db.execute("SELECT username, password, user_id, authority, nickname FROM users WHERE username = ?", params["username"])
+        db = database()
+        result = db.execute("SELECT username, password, user_id, authority, nickname FROM users WHERE username = ?", params["username"])
         if result.length > 0
             if BCrypt::Password.new(result[0]["password"]) == params["password"] && result[0]["username"] == params["username"]
                 return{
@@ -16,31 +16,30 @@ module AppModule
                     user: params["username"]
                 }
             else
-                notice_login: "There was an error when logging in"
+                flash[:notice_login] = "There was an error when logging in"
             end
         end
     end
 
     def create(params)
-    db = database()
-    new_name = params["username"] 
-    new_password = params["password"]
-    new_password_hash = BCrypt::Password.create(new_password)
-    db.execute("INSERT INTO users (username, password, authority, nickname) VALUES (?,?,?,?)", new_name, new_password_hash, 1)
-    result = db.execute("SELECT * FROM users")
-        return {
+        db = database()
+        new_name = params["username"] 
+        new_password = params["password"]
+        new_password_hash = BCrypt::Password.create(new_password)
+        db.execute("INSERT INTO users (username, password, authority, nickname) VALUES (?,?,?,?)", new_name, new_password_hash, 1)
+        result = db.execute("SELECT * FROM users")
+        if result > 0
+            return {
             user_id: result[0]["user_id"],
             user: params["username"]
-        }
-    else
-        notice_create: "There was an error creating the account"
-        }
+            }
+        else
+            flash[:notice_create] = "There was an error creating the account"
+        end
     end 
 
     def import_img_1A()
         Dir.glob('/public/img/1A/*.jpg') do |img|
-            next if img = ".." || img = "."
-            images_1A = []
             session[:images_1A] = images_1A.append(img)
         end
     end
@@ -59,58 +58,39 @@ module AppModule
 
     def insert_img_to_db(student_id) 
         session[:images_1A].each do |query|
-        query = <<-SQL 
-        INSERT INTO images
-        img, student_id
-        VALUES ?,?
-        query, student_id
-        db.execute(query, session[:student_id])
-        SQL
+            query = <<-SQL 
+            INSERT INTO images
+            img, student_id
+            VALUES ?,?
+            query, student_id
+            db.execute(query, session[:student_id])
+            SQL
+        end
     end 
 
     def insert_img_to_db(student_id) 
         session[:images_2A].each do |query|
-        query = <<-SQL 
-        INSERT INTO images
-        img, student_id
-        VALUES ?,?
-        query, student_id
-        db.execute(query, session[:student_id])
-        SQL
+            query = <<-SQL 
+            INSERT INTO images
+            img, student_id
+            VALUES ?,?
+            query, student_id
+            db.execute(query, session[:student_id])
+            SQL
+        end
     end 
 
     def insert_img_to_db(student_id) 
         session[:images_3A].each do |query|
-        query = <<-SQL 
-        INSERT INTO images
-        img, student_id
-        VALUES ?,?
-        query, student_id
-        db.execute(query, session[:student_id])
-        SQL
+            query = <<-SQL 
+            INSERT INTO images
+            img, student_id
+            VALUES ?,?
+            query, student_id
+            db.execute(query, session[:student_id])
+            SQL
+        end
     end 
-
-
-
-
-    
-        
-
-
-
-
-
-    
-
-
-        
-            
-            
-
-
-
-
-                
 
     def change_username(params, user_id) 
         db = database()
@@ -121,8 +101,4 @@ module AppModule
         db = database()
         db.execute("UPDATE users SET password = ? WHERE user_id = ?", params['change_password'], user_id)
     end
-
-    
-
-end 
-
+end
