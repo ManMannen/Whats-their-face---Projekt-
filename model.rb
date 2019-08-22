5dc1767263dcf4645fac7c7ad4927bcae9782250
Oskar <oskar@pilborg.se>
2019-08-22 13:18:35 +0200

    Update model.rb

--- model.rb
+++ model.rb
@@ -14,7 +14,7 @@
         if result.length > 0
             if BCrypt::Password.new(result[0]["password"]) == params["password"] && result[0]["first_name"] == params["first_name"]
                 {
-                student_id: result[0]["student_id"],
+                teacher_id: result[0]["teacher_id"],
                 first_name: params["first_name"]
                 }
             else
@@ -29,7 +29,6 @@
         last_name = params["last_name"]
         password = params["password"]
         password_hash = BCrypt::Password.create(password)
-        byebug
         result = db.execute("SELECT * FROM teachers")
         if result > 0
             flash[:notice_create] = "There was an error creating the account"
@@ -38,12 +37,12 @@
         end
     end
 
-    def change_username(params, student_id) 
+    def change_username(params, teacher_id) 
         db = database()
-        db.execute("UPDATE users SET username = ? WHERE student_id = ?", params['change_username'], student_id)
+        db.execute("UPDATE teachers SET first_name = ? WHERE teacher_id = ?", params['change_username'], teacher_id)
     end
     
-    def change_password(params, student_id) 
+    def change_password(params, teacher_id) 
         db = database()
-        db.execute("UPDATE users SET password = ? WHERE student_id = ?", params['change_password'], student_id)
+        db.execute("UPDATE teacher SET password = ? WHERE teacher_id = ?", params['change_password'], teacher_id)
     end
