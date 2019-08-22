3bfb3940a16d45a30b8caaaaaf9564f8afeb5e89
ManMannen <alexander.nilssonmelbostad@gmail.com>
2019-08-22 09:22:05 +0200

    Update import.rb

--- import.rb
+++ import.rb
@@ -1,18 +1,28 @@
 def import()
     Dir.glob("import/*.jpg") do |img|
-        split_info = img.split(" ")
+        split_info = img.split(" ") #Splittar upp informationen från bilden
         db = database()
-        db.execute(INSERT INTO students Values ?,?)
-
-
-
-
-        
-        
-        
-        #extract klass, name from image filename
-        #SQL insert into students klass, name
-        #SQL select latest inserted students id
-        #rename (move) image to students id
+        db.execute(
+        "INSERT INTO
+        students
+        (klass, first_name, last_name)
+        Values (?,?,?)",
+        split_info[0], split_info[1], split_info[2]
+        ) #Skickar in informationen från bild till Server
+        result = db.execute("
+        SELECT student_id
+        FROM 
+        students
+        WHERE 
+        student_id = (SELECT MAX(student_id) 
+        FROM students)
+        ") # Hittar senaste student_id
+        begin
+            File.exist?("/import")
+                FileUtils.mv("img/import, img/class")
+        rescue 
+            "The file couldn't be moved"
+        end
+        File.rename(img, "#{result} + ".jpg"") --# Byter namn till senaste student_id
     end
 end
